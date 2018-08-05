2546473b66114d27495ef493b57736894355b2c2
ceramic <ceramic.yu@foxmail.com>
2018-08-04 19:53:30 -0700

    dsd

--- api/main.go
+++ api/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
+func RegisterHandlers()*httprouter.Router{
+   router:=httprouter.New()
+   router.POST("/user",CreatUser)
+   return router
+}
 func main (){
-
+  r:=RegisterHandlers()
+  http.ListenAndServe(":8000",r)
 }
