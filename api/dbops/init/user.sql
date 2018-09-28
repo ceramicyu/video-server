f520a230f82849a22cd4a5a86d5df4fcb91ae8a7
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2018-09-27 22:13:54 -0700

    Create user.sql

---
+++ api/dbops/init/user.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` char(20) NOT NULL DEFAULT '' COMMENT '账号',
+  `password` char(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `create_time` int(11) NOT NULL DEFAULT '0',
+  `update_time` int(11) NOT NULL DEFAULT '0',
+  `locked` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否锁定',
+  `accesstype` int(11) NOT NULL DEFAULT '1' COMMENT '管理员权限',
+  `phone` varchar(20) NOT NULL DEFAULT '',
+  `login_ip` bigint(12) NOT NULL DEFAULT '0',
+  `last_ip` int(11) NOT NULL DEFAULT '0' COMMENT '上次登录的ip',
+  `lock_ip` varchar(255) NOT NULL DEFAULT '' COMMENT '登录锁定ip',
+  `lock_time` int(11) NOT NULL DEFAULT '0' COMMENT '锁定时间',
+  `lock_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '登录锁',
+  `login_time` int(11) NOT NULL DEFAULT '0' COMMENT '登录时间',
+  `last_time` int(11) NOT NULL DEFAULT '0' COMMENT '上次登录时间',
+  `level` tinyint(2) NOT NULL DEFAULT '99' COMMENT '权值',
+  `token` char(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `username` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
