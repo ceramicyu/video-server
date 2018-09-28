369fcbf26508d2ad054e7e7fd8e18f72cf6a98eb
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2018-09-27 22:19:21 -0700

    Update user.sql

--- api/dbops/init/user.sql
+++ api/dbops/init/user.sql
@@ -19,3 +19,5 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`) USING BTREE,
   KEY `username` (`username`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+INSERT INTO `user`(`id`, `username`, `password`, `create_time`, `update_time`, `locked`, `accesstype`, `phone`, `login_ip`, `last_ip`, `lock_ip`, `lock_time`, `lock_status`, `login_time`, `last_time`, `level`, `token`) VALUES (1, 'qweqwe', 'ecdbee68c6c3c30827abea1c43a5f23b', 1535710791, 1535710791, 1, 1, '', 2886795265, 2147483647, '3232243201', 1534233658, 0, 1535710791, 1535528380, 0, 'dc702ed41bef22d029cddcfaeaeba087');
