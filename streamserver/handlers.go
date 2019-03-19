4a9bcfa9a12a6ff4db89f0e43f0d532386bb4927
ceramicyu <31750600+ceramicyu@users.noreply.github.com>
2019-03-19 19:30:20 +0800

    Add files via upload

--- streamserver/handlers.go
+++ streamserver/handlers.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"os"
+	"time"
+	"io/ioutil"
+	"io"
+	"html/template"
+)
+
+func streamHandler(w http.ResponseWriter,r *http.Request,p httprouter.Params){
+	vid:=p.ByName("vid-id")
+	vl:=VIDEO_DIR+vid
+	vidoe,err:=os.Open(vl)
+	if err!=nil{
+		sendErrorResponse(w,http.StatusInternalServerError," inner err.")
+	   return
+	}
+	w.Header().Set("Content_Type","video/mp4")
+	http.ServeContent(w,r,"",time.Now(),vidoe)
+	defer vidoe.Close()
+}
+
+func uploadHandler(w http.ResponseWriter,r *http.Request,p httprouter.Params){
+	r.Body=http.MaxBytesReader(w,r.Body,MAX_UPLOAD_SIZE)
+	if err:=r.ParseMultipartForm(MAX_UPLOAD_SIZE);err!=nil{
+		sendErrorResponse(w,http.StatusBadRequest,"bad request file is too big")
+		return
+	}
+	file,_,err:=r.FormFile("file")
+	if err !=nil{
+		sendErrorResponse(w,http.StatusInternalServerError,"read form err")
+		return
+	}
+	data,err:=ioutil.ReadAll(file)
+	if err!=nil{
+	   sendErrorResponse(w,http.StatusInternalServerError," ioutil err")
+	}
+	filename:=p.ByName("vid-id")
+	err=ioutil.WriteFile(VIDEO_DIR+filename,data,0666)
+	if err!=nil{
+		sendErrorResponse(w,http.StatusInternalServerError,"write err")
+	}
+	w.WriteHeader(http.StatusCreated)
+    io.WriteString(w,"upload succesfully.")
+
+
+}
+
+func testPageHandler(w http.ResponseWriter,r *http.Request ,p httprouter.Params){
+	t,_:=template.ParseFiles(HTML_DIR+"upload.html")
+	t.Execute(w,nil)
+
+//////////////////////
+}
--- streamserver/main.go
+++ streamserver/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-  "io"
-  "net/http"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+func NewMiddleWareHandler(r *httprouter.Router,cc int)http.Handler{
+	m:=middleWareHandler{}
+	m.r=r
+	m.l=NewConnLimiter(cc)
+	return m.r
+}
+
+func RegisterHandlers()*httprouter.Router{
+	router:=httprouter.New()
+
+	router.GET("/videos/:vid-id",streamHandler)
+	router.POST("/upload/:vid-id",uploadHandler)
+
+	router.GET("/testpage",testPageHandler)
+
+	return router
+}
+
+func (m *middleWareHandler)ServerHTTP(w http.ResponseWriter,r *http.Request){
+	if !m.l.GetConn(){
+		sendErrorResponse(w,http.StatusTooManyRequests,"too many requrest .")
+		return
+	}
+	m.r.ServeHTTP(w,r)
+	defer m.l.ReleaseConn()
+}
+
 func main(){
-  http.HandleFunc("/user", func(writer http.ResponseWriter, request *http.Request) {
-    io.WriteString(writer,"this a user page")
-  })
-  http.ListenAndServe(":80",nil)
+	r:=RegisterHandlers()
+	mh:=NewMiddleWareHandler(r,2)
+	http.ListenAndServe(":8080",mh)
 }
